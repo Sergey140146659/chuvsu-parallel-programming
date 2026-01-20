740eab084fddda6de5627ce7068a9d2e225858aa
Sergei Shcherbakov <sergey14014@yandex.ru>
2026-01-21 02:23:58 +0300

    task6 complete

---
+++ task6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <format>
+#include <omp.h>
+
+int main() {
+    int k;
+    long long N;
+    std::cin >> k >> N;
+    long long sum = 0;
+    #pragma omp parallel num_threads(k) reduction(+:sum)
+    {
+        int rank = omp_get_thread_num();
+        long long current_sum = 0;
+
+        #pragma omp for
+        for (long long i = 1; i <= N; ++i) {
+            current_sum += i;
+        }
+        sum += current_sum;
+
+        std::cout << std::format("[{}]: Sum = {}\n", rank, current_sum);
+    }
+
+    std::cout << std::format("Sum = {}", sum);
+}
